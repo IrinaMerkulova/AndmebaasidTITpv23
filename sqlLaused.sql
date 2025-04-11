924dfa14051ae7d5e696e94c55e6c30c5a785cbb
IrinaMerkulova <irina.merkulova@gmail.com>
2025-04-11 13:41:24 +0300

    Update sqlLaused.sql

--- sqlLaused.sql
+++ sqlLaused.sql
@@ -29,3 +29,40 @@ INSERT INTO opilane(eesnimi, perenimi, synniaeg, aadress, opilaskodu)
 VALUES ('Andrei', 'Ivanov', '2005-12-5', 'Tallinn', 1),
 ('Mati', 'Kask', '2003-12-5', 'Tallinn', 0),
 ('Peeter', 'Uus', '2000-10-5', 'Tallinn', 0);
+
+CREATE TABLE ryhm(
+ryhmID int not null primary key identity(1,1),
+ryhm varchar(10) unique,
+osakond varchar(20)
+);
+INSERT INTO ryhm(ryhm, osakond)
+Values('TITpv24', 'IT'),('KRRpv23','Rätsepp');
+
+Select * from ryhm;
+--lisame uus veerg RyhmID tabelisse opilane
+ALTER TABLE opilane ADD ryhmID int;
+
+Select *  from opilane;
+
+--lisame foreign key veergule ryhmID mis on seotud
+--tabeliga ryhm ja veerguga ryhmID
+ALTER TABLE opilane 
+Add foreign key (ryhmID) references ryhm(ryhmID);
+
+--foreign key kontroll
+INSERT INTO opilane
+(eesnimi, perenimi, synniaeg, aadress, opilaskodu, ryhmID)
+VALUES ('Andrei', 'Ivanovssss', '2005-12-5', 'Tallinn', 1, 3);
+
+SELECT * FROM opilane;
+--kasutame seos tabelite vahel - JOIN
+SELECT * FROM opilane JOIN ryhm
+ON opilane.ryhmID=ryhm.ryhmID;
+
+SELECT opilane.perenimi, ryhm.ryhm FROM opilane JOIN ryhm
+ON opilane.ryhmID=ryhm.ryhmID;
+
+--lihtsaim vaade
+SELECT o.perenimi, r.ryhm, o.aadress
+FROM opilane o JOIN ryhm r
+ON o.ryhmID=r.ryhmID;
